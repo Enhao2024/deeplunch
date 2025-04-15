f5a6fe4418797b0dd5c215d3609db2c907b469ea
Songyang <songyangtian44@gmail.com>
2025-04-15 20:15:58 +1200

    NSO-47 update rian's profile and menu
    NSO-47 Update Menu name of Home

--- utils/MenuConfig.ts
+++ utils/MenuConfig.ts
@@ -1,8 +1,26 @@
-import { CheckSquare, Clipboard, DollarSign, Github, Grid, Heart, Home, Icon, Lambda, Linkedin, Mail, MapPin, MessageSquare, Moon, Zap } from "@geist-ui/icons";
+import {
+  CheckSquare,
+  Clipboard,
+  DollarSign,
+  Github,
+  Grid,
+  Heart,
+  Home,
+  Icon,
+  Lambda,
+  Linkedin,
+  Mail,
+  MapPin,
+  MessageSquare,
+  Moon,
+  Share,
+  Zap,
+  Codepen
+} from "@geist-ui/icons";
 import { AppConfig } from "./AppConfig";
 
 export enum MenuTitle {
-  HOME = "hOME",
+  HOME = "Home",
   DASHBOARD = "Dashboard",
   GIT_REPO = "GitHub",
   CHAT_ROOM = "Chat Room",
@@ -19,38 +37,39 @@ export enum MenuTitle {
   ROADMAP = "RoadMap",
   PLANNER = "Planner",
   THANKS = "Thanks",
-  MEDITATION = "Meditation"
+  MEDITATION = "Meditation",
+  BRAINSTORM="BrainStorm"
 }
 
 export interface Menu {
-  title: MenuTitle,
-  href: string,
-  golive: boolean,
-  icon: Icon,
+  title: MenuTitle;
+  href: string;
+  golive: boolean;
+  icon: Icon;
 }
 
 export const MenuConfig: { [key: string]: Menu } = {
   [MenuTitle.HOME]: {
     title: MenuTitle.HOME,
-    href: '/',
+    href: "/",
     golive: true,
     icon: Home,
   },
   [MenuTitle.ROADMAP]: {
     title: MenuTitle.ROADMAP,
-    href: '/roadmap',
+    href: "/roadmap",
     golive: true,
     icon: Lambda,
   },
   [MenuTitle.PLANNER]: {
     title: MenuTitle.PLANNER,
-    href: '/planner',
+    href: "/planner",
     golive: true,
     icon: CheckSquare,
   },
   [MenuTitle.THANKS]: {
     title: MenuTitle.THANKS,
-    href: '/thanks',
+    href: "/thanks",
     golive: true,
     icon: Heart,
   },
@@ -62,85 +81,93 @@ export const MenuConfig: { [key: string]: Menu } = {
   },
   [MenuTitle.DASHBOARD]: {
     title: MenuTitle.DASHBOARD,
-    href: '/dashboard',
+    href: "/dashboard",
     golive: true,
     icon: Grid,
   },
   [MenuTitle.LUNCH_RECIPE]: {
     title: MenuTitle.LUNCH_RECIPE,
-    href: '/recipe',
+    href: "/recipe",
     golive: false,
     icon: Clipboard,
   },
   [MenuTitle.AFTER_WORK_PLAN]: {
     title: MenuTitle.AFTER_WORK_PLAN,
-    href: '/after_work_plan',
+    href: "/after_work_plan",
     golive: false,
     icon: CheckSquare,
   },
   [MenuTitle.AFTERNOON_PLAN]: {
     title: MenuTitle.AFTERNOON_PLAN,
-    href: '/afternoon_plan',
+    href: "/afternoon_plan",
     golive: false,
     icon: CheckSquare,
   },
   [MenuTitle.WEEKEND_PLAN]: {
     title: MenuTitle.WEEKEND_PLAN,
-    href: '/weekend_plan',
+    href: "/weekend_plan",
     golive: false,
     icon: CheckSquare,
   },
   [MenuTitle.LEARNING]: {
     title: MenuTitle.LEARNING,
-    href: '/learning',
+    href: "/learning",
     golive: false,
     icon: Zap,
   },
   [MenuTitle.NEXT_WEEK_PLAN]: {
     title: MenuTitle.NEXT_WEEK_PLAN,
-    href: '/next_week_plan',
+    href: "/next_week_plan",
     golive: false,
     icon: CheckSquare,
   },
   [MenuTitle.CHAT_ROOM]: {
     title: MenuTitle.CHAT_ROOM,
-    href: '/chatroom',
+    href: "/chatroom",
     golive: false,
     icon: MessageSquare,
   },
   [MenuTitle.ABOUT]: {
     title: MenuTitle.ABOUT,
-    href: '/about',
+    href: "/about",
     golive: true,
     icon: Linkedin,
   },
   [MenuTitle.LUNCH_GUESS]: {
     title: MenuTitle.LUNCH_GUESS,
-    href: '/lunch',
+    href: "/lunch",
     golive: true,
     icon: MapPin,
   },
   [MenuTitle.CONTACT]: {
     title: MenuTitle.CONTACT,
-    href: '/contact',
+    href: "/contact",
     golive: true,
     icon: Mail,
   },
   [MenuTitle.DONATE]: {
     title: MenuTitle.DONATE,
-    href: '/donate',
+    href: "/donate",
     golive: false,
     icon: DollarSign,
   },
   [MenuTitle.MEDITATION]: {
     title: MenuTitle.MEDITATION,
-    href: '/meditation',
+    href: "/meditation",
     golive: false,
     icon: Moon,
+  },
+  [MenuTitle.BRAINSTORM]: {
+    title: MenuTitle.BRAINSTORM,
+    href: "/brainstorm",
+    golive: true,
+    icon: Codepen,
   },
 };
 
 export const TOP_BAR_MENU = [
+  MenuConfig[MenuTitle.HOME],
+  MenuConfig[MenuTitle.DASHBOARD],
   MenuConfig[MenuTitle.GIT_REPO],
   MenuConfig[MenuTitle.ROADMAP],
   MenuConfig[MenuTitle.THANKS],
@@ -152,11 +179,27 @@ export const LEFT_BAR_MENU = [
   MenuConfig[MenuTitle.DASHBOARD],
   MenuConfig[MenuTitle.LUNCH_GUESS],
   MenuConfig[MenuTitle.PLANNER],
+  MenuConfig[MenuTitle.BRAINSTORM],
   MenuConfig[MenuTitle.MEDITATION],
   MenuConfig[MenuTitle.LEARNING],
   MenuConfig[MenuTitle.CHAT_ROOM],
-]
+];
 
+export const MOBILE_LEFT_BAR_MENU = [
+  MenuConfig[MenuTitle.HOME],
+  MenuConfig[MenuTitle.DASHBOARD],
+  MenuConfig[MenuTitle.ROADMAP],
+  MenuConfig[MenuTitle.LUNCH_GUESS],
+  MenuConfig[MenuTitle.PLANNER],
+  MenuConfig[MenuTitle.BRAINSTORM],
+  MenuConfig[MenuTitle.MEDITATION],
+  MenuConfig[MenuTitle.LEARNING],
+  MenuConfig[MenuTitle.CHAT_ROOM],
+  MenuConfig[MenuTitle.GIT_REPO],
+  MenuConfig[MenuTitle.THANKS],
+  MenuConfig[MenuTitle.ABOUT],
+  MenuConfig[MenuTitle.CONTACT],
+];
 export const PROTECTED_PAGE = [
   MenuConfig[MenuTitle.DASHBOARD],
   MenuConfig[MenuTitle.LUNCH_GUESS],
@@ -164,7 +207,7 @@ export const PROTECTED_PAGE = [
   MenuConfig[MenuTitle.MEDITATION],
   MenuConfig[MenuTitle.LEARNING],
   MenuConfig[MenuTitle.CHAT_ROOM],
-]; 
+];
 
 export const NON_FUNCTIONAL_PAGE = [
   MenuConfig[MenuTitle.HOME],
@@ -173,4 +216,4 @@ export const NON_FUNCTIONAL_PAGE = [
   MenuConfig[MenuTitle.ABOUT],
   MenuConfig[MenuTitle.ROADMAP],
   MenuConfig[MenuTitle.THANKS],
-]
+];
